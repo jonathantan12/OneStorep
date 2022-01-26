0eed04261fc4a8f10ff522e8b81222682255c84f
jonathantan12 <74011226+jonathantan12@users.noreply.github.com>
2022-01-27 02:36:04 +0800

    Update the filters

--- BusinessDashboard/assets/js/main.js
+++ BusinessDashboard/assets/js/main.js
@@ -375,7 +375,7 @@ function filterDeliveredDate() {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[11];
+    td = tr[i].getElementsByTagName("td")[10];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
